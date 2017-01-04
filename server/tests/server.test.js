c30ece0024d0583d8e4c0569266705309352d84f
MinhHoang <mh0132345@gmail.com>
2017-01-05 01:25:49 +0200

    Add test for GET/ todo by ID

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 var {app} = require('./../server.js');
 var {Todo} = require('./../models/todo.js');
 var {User}= require('./../models/user.js');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test To do'
 }, {
+  _id: new ObjectID(),
   text: 'Second test To do'
 }];
 
@@ -66,4 +69,31 @@ describe("GET/ todo", () => {
       })
       .end(done);
     });
+});
+
+describe("GET/ todo ID", () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res)  => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found',(done) => {
+    var hexID = new ObjectID().toHexString();
+    request(app)
+    .get(`/todos/${hexID}`)
+    .expect(404)
+    .end(done);
+  });
+
+  it('should return 404 if ID not valid',(done) => {
+    request(app)
+    .get(`/todos/123`)
+    .expect(404)
+    .end(done);
+  });
 });
